df9775c69fe59aa725ac2f094c616fd5a9598938
Fan Siew Ying <62295496+fansiewying99@users.noreply.github.com>
2021-05-04 02:59:13 +0900

    Ticket class
    variables: ticketId and purchaseTimestamp

---
+++ Ticket.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Ticket {
+
+    public String ticketId;
+    public String buyTime;
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setBuyTime(String buyTime) {
+        this.buyTime = buyTime;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public String getBuyTime() {
+        return buyTime;
+    }
+
+}
