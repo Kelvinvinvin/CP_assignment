849a10b1ac77cb6f642cd16cd5c0301c0df97d23
chenbg123 <chennice123@163.com>
2021-05-04 19:27:14 +0800

    r

--- src4/src/entrance.java
+++ src4/src/entrance.java
@@ -1,4 +1,3 @@
-
 import java.util.Queue;
 import java.util.Random;
 import java.util.Stack;
@@ -12,19 +11,18 @@ public class entrance{
 		
 		int maxSize,curVistor,ticketNum;
         String direction;
-        MuseumCon museum;
+        Museum museum;
         TicketSystem ticket;
        // Turnstile[]turnstile=new Turnstile[4];
-		volatile Queue <turnstile> turnStileQueue=new Queue<>(4);
+		volatile Queue <Ticket> ticketQueue=new LinkedList<>();
 
-       // openTime= (int) Time.valueOf("09:00:00").getTime();
-		//closeTime= (int) Time.valueOf("18:00:00").getTime();
-        
+    
 		
-		public entrance(int ticketNum,String direction){
+		public entrance(String direction,TicketSystem ticket){
 			this.maxSize=4;
             this.direction=direction;
-            this.ticketNum=ticketNum;
+            this.ticket=ticket;
+          //  this.ticketNum=ticketNum;
 			this.lock=new ReentrantLock();
 			this.notEmpty=lock.newCondition();
 			this.notFull=lock.newCondition();
@@ -34,58 +32,30 @@ public class entrance{
 
 
         public void enterMusem(){
-            Random random =new Random();
-            int turnStileId=random.nextInt(4)+1;
-            String id=direction+"E"+turnStileId;
-
-
-            while(turnStileQueue.size()){
-
-            }
-
-            for(int a=0;a<this.ticketNum;a++){
-                turnstile=new turnstile(museum,ticket, id);
-            }
-
-			try {
-				
-				while(stack.size()>=maxSize) {
-			        System.out.println("Thread " + Thread.currentThread().getId() + ": stack is full, waiting to read.stack:"+stack);
-					notFull.await();
-				}
-				
-				stack.push(o);
-				System.out.println("Thread " + Thread.currentThread().getId()+": finished writing "+o+" to the stack. satch:"+stack);
-				notEmpty.signal();
-				
-			}finally {
-				lock.unlock();
-
-			}
+        
         }
 
 		
 
- 
     class turnstile extends Thread{
         int curVistor;
         Mesume museum;
         String turnstileId;
 
         turnstile(Musem museum,TicketSystem ticket,String turnstileId){
-            this.museum=mueseum;
+            this.museum=museum;
             this.turnstileId=turnstileId;
         }
             
         @Override
         public void run() {
                 try{
-                    while (mueseum.curVistor<mueseum.curMaxVisit) {
+                    //while (mueseum.curVistor<mueseum.curMaxVisit) {
                         Time time=new Time((int) LocalTime.now().getNano());
                         System.out.println(time+" Ticket "+ticket.id+ " entered through Turnstile"+turnstileId+" Staying for "+mueseu.getVistorTime); 
                         museum.curVisitor++;
                         Thread.sleep(500);      
-                    }
+                   // }
                  }catch (InterruptedException e) {
                     e.printStackTrace();
                 }
