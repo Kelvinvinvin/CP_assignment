798a83437056bb70d71a28245ec2d8ff68535e72
chenbg123 <chennice123@163.com>
2021-05-04 19:14:56 +0800

    museum

--- src4/src/Museum.java
+++ src4/src/Museum.java
@@ -1,4 +1,5 @@
 import java.text.ParseException;
+import java.util.Random;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -10,9 +11,29 @@ public class Museum {
     private String closeTime;
     DateTimeFormatter formatter;
 
+
+    private int capacity,curMax,curVistor;
+	public static boolean canEnter = false;
+	public static boolean canExit = false;
+	public long duration,arrivalTime,leaveTime;
+
+
+
+    MuseumControl(int arrivalTime){
+		this.capacity=900;
+		this.curMax=100;
+		this.curVistor=0;
+		this.duration=getVistorTime();
+		this.leaveTime=getLeavelTime();
+		this.arrivalTime=arrivalTime;
+		
+	}
+	
+
     //for daily maximum visitors limitation
     private int maxVisit;
     private int curMaxVisit;
+
 
     //for current visitor count
     private int curVisitor;
@@ -22,13 +43,16 @@ public class Museum {
     public Museum() {
         this.maxVisit = 900;
         this.curMaxVisit = 100;
+
         this.maxVisitor = 0;
         this.curVisitor = 0;
+
         this.formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         this.openTime = "9:00:00";
         this.closeTime = "21:00:00";
 
     }
+    
     public int getCurVisitor() {
         return curVisitor;
     }
@@ -44,6 +68,18 @@ public class Museum {
     public int getMaxVisit() {
         return this.maxVisit;
     }
+
+    public long getVistorTime() {
+        Random  random=new Random();
+		int duration=random.nextInt(101*60*1000)+(50*60*1000);
+		return duration;
+
+    }
+    
+    public long getLeavelTime() {	
+    	return (this.arrivalTime+getVistorTime());    
+    }
+
 
     public boolean isOpen() {
         if (isTimeWith_in_Interval((LocalTime.now()).format(formatter),openTime,closeTime)) {
@@ -71,18 +107,11 @@ public class Museum {
         return isBetween;
     }
 
-   
+  
 
     public String getCurTime(){
         return (LocalTime.now()).format(formatter);
     }
-    
-    //exit 
-    exit QuitEastExit1 = new exit();
-    exit QuitEastExit2 = new exit();
-    exit QuitEastExit3 = new exit();
-    exit QuitEastExit4 = new exit();
-    
 
 }
 
